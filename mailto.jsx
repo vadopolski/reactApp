a8209b265275ba4f5d1aca44bce404c3144787c2
vadopolski <vop444@gmail.com>
2018-05-27 11:05:57 +0300

    Added second filds with event.

--- mailto.jsx
+++ mailto.jsx
@@ -5,7 +5,7 @@ function InputSubmit() {
         <div>
             <label>&nbsp;</label>
             <div>
-                <input type="Submit" name="name"/>
+                <input type="submit" value="Submit" />
             </div>
         </div>
     )
@@ -14,9 +14,9 @@ function InputSubmit() {
 function InputText(props) {
     return (
         <div>
-            <label for={props.name}>{props.value}</label>
+            <label for={props.name}>{props.labelName}</label>
             <div>
-                <input type="text" name={props.name} id={props.name}/>
+                <input type="text" onChange={props.onchange} id={props.name} value={props.value}/>
             </div>
         </div>
     )
@@ -25,36 +25,56 @@ function InputText(props) {
 class MailTo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {subjectValue: ''};
+        this.state = {bodyValue: ''};
+        this.state = {subject: 'subject1'};
+        this.state = {body: 'body'};
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChange1 = this.handleChange1.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
+    handleChange1(event) {this.setState({subjectValue: event.target.value});}
+    handleChange2(event) {this.setState({bodyValue: event.target.value});}
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+        alert('A name, email was submitted: '  + this.state.subjectValue + ' ' + this.state.subject + ' ' +
+            this.state.body + ' ' + this.state.bodyValue);
         event.preventDefault();
     }
 
     render() {
         return (
-            <div onSubmit={this.handleSubmit}>
+            <form onSubmit={this.handleSubmit}>
                 <h2>Письмо посетителя сайта.</h2>
-                <InputText value="Куда" name="where"/>
-                <InputText value="Тема" name="subject"/>
-                <InputText value="Тема" name="body"/>
+                <InputText labelName="Subject:" onchange={this.handleChange1} name={this.state.subject} value={this.state.subjectValue}/>
+                <InputText labelName="Body:" onchange={this.handleChange2} name={this.state.body} value={this.state.bodyValue}/>
                 <h2>Координаты для связи.</h2>
-                <InputText value="Фамилия, имя, отчество " name="city"/>
-                <InputText value="Город " name="phone"/>
-                <InputText value="Телефон с кодом города " name="email"/>
-                <InputSubmit/>
-            </div>
+                {/*<InputText labelName="City:" onchange={this.handleChange2} name="city" value={this.state.subjectBody}/>*/}
+                {/*<InputText labelName="Phone:" onchange={this.handleChange2} name="phone" value={this.state.subjectBody}/>*/}
+                {/*<InputText labelName="Email:" onchange={this.handleChange2} name="email" value={this.state.subjectBody}/>*/}
+                <input type="submit" value="Submit"/>
+            </form>
         );
     }
+
+    // render() {
+    //     return (
+    //         <div onSubmit={this.handleSubmit}>
+    //             <h2>Письмо посетителя сайта.</h2>
+    //             <InputText value={this.state.value} onChange={this.handleChange} name="where"/>
+    //             <InputText value={this.state.value} onChange={this.handleChange} name="subject"/>
+    //             <InputText value={this.state.value} onChange={this.handleChange} name="body"/>
+    //             <h2>Координаты для связи.</h2>
+    //             <InputText value={this.state.value} onChange={this.handleChange} name="city"/>
+    //             <InputText value={this.state.value} onChange={this.handleChange} name="phone"/>
+    //             <InputText value={this.state.value} onChange={this.handleChange} name="email"/>
+    //             <InputSubmit/>
+    //         </div>
+    //     );
+    // }
+
 }
 
 export default MailTo;
